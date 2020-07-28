11db8c7f64929bbf46850a6c14a0701010807a42
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2020-07-28 23:11:02 +0100

    version 1.0.3

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'mainLayout', layoutsDi
 const rosterRoute = require('./routes/routes')
 app.use('/', rosterRoute)
 
-http.listen((process.env.PORT || 5000), function(){
-    console.log('listening on *:5000');
+http.listen((process.env.PORT || 3000), function(){
+    console.log('listening on *:3000');
   });
